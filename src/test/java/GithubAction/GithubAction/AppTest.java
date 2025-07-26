54391b9a9baf70039df4ab286634a3baf32610e9
Aniket Singh <aniketsingh2704@gmail.com>
2025-07-26 20:29:36 +0530

    .gitignore added

--- src/test/java/GithubAction/GithubAction/AppTest.java
+++ src/test/java/GithubAction/GithubAction/AppTest.java
@@ -1,5 +1,6 @@
 package GithubAction.GithubAction;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,8 @@ public class AppTest {
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testMessage() {
+        String expected = "Hello World";
+        assertEquals(expected, App.getMessage());
     }
 }
